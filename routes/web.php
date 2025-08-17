a33f12c5a820db80f8682ac67683e8caa8e2041d
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-08-17 12:43:52 -0500

    refactor: remove Category management components and routes

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 
-    Route::get('dashboard/categories', [CategoryController::class, 'index'])->name('categories.index');
-    Route::get('dashboard/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
-    Route::get('dashboard/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-    Route::post('dashboard/categories', [CategoryController::class, 'store'])->name('categories.store');
-    Route::get('dashboard/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-    Route::put('dashboard/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
-    Route::delete('dashboard/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
-
     Route::get('dashboard/places', [PlaceController::class, 'index'])->name('places.index');
     Route::get('dashboard/places/create', [PlaceController::class, 'create'])->name('places.create');
     Route::post('dashboard/places', [PlaceController::class, 'store'])->name('places.store');
