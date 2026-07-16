eb63356db05a9c805125aa306831a3f28809074e
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2026-07-16 08:52:09 -0500

    feat(checklist): register Checklist and ChecklistItem routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\business\CategoryController;
+use App\Http\Controllers\business\ChecklistController;
+use App\Http\Controllers\business\ChecklistItemController;
 use App\Http\Controllers\business\PlaceController;
 use App\Http\Controllers\business\ProductController;
 use App\Http\Controllers\business\UnitController;
@@ -28,6 +30,20 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
         // Products routes - Using resource controller
         Route::resource('products', ProductController::class)->except(['show']);
+
+        // Checklists routes
+        Route::get('checklists', [ChecklistController::class, 'index'])->name('checklists.index');
+        Route::get('checklists/active', [ChecklistController::class, 'active'])->name('checklists.active');
+        Route::post('checklists', [ChecklistController::class, 'store'])->name('checklists.store');
+        Route::get('checklists/{checklist}', [ChecklistController::class, 'show'])->name('checklists.show');
+        Route::post('checklists/{checklist}/complete', [ChecklistController::class, 'complete'])->name('checklists.complete');
+        Route::post('checklists/{checklist}/cancel', [ChecklistController::class, 'cancel'])->name('checklists.cancel');
+
+        // Checklist items routes
+        Route::post('checklists/{checklist}/items', [ChecklistItemController::class, 'store'])->name('checklist-items.store');
+        Route::delete('checklists/{checklist}/items/{item}', [ChecklistItemController::class, 'destroy'])->name('checklist-items.destroy');
+        Route::patch('checklist-items/{item}/mark-bought', [ChecklistItemController::class, 'markBought'])->name('checklist-items.mark-bought');
+        Route::patch('checklist-items/{item}/mark-not-bought', [ChecklistItemController::class, 'markNotBought'])->name('checklist-items.mark-not-bought');
     });
 });
 
